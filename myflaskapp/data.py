5cc2f71e4a11b0f22a39d464bab374d743dccaab
Kevin <Kevin@172-16-14-96.DYNAPOOL.NYU.EDU>
2017-11-15 11:09:02 -0500

    First Commit

---
+++ myflaskapp/data.py
@@ -0,0 +1,22 @@
+def prices():
+	prices = [
+		{
+			'id': 'trial',
+			'type': 'Trial',
+			'desc': 'Try PKR free for two weeks!',
+			'price': '0.00'
+		},
+		{
+			'id': 'business',
+			'type': 'Business',
+			'desc': 'The plan perfect for businesses with one or two stores.',
+			'price': '19.99'
+		},
+		{
+			'id': 'business-lg',
+			'type': 'Large businesses',
+			'desc': 'Want to step up your management? This plan is for companies who need to manage multiple stores!',
+			'price': '29.99'
+		}
+	]
+	return prices;
